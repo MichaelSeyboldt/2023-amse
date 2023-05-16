702c9049f85a2d15025b77a1894cdc912910c339
Michael Seyboldt <MichaelSeyboldt@users.noreply.github.com>
2023-05-16 17:01:48 +0200

    Feat piplines (#10)
    * start adding a data fetching pipeline
    
    * added download script for src 2
    
    * convesion to sql for datasource 1
    
    * add conversion script for datasource 2
    
    * update gitignore to ignore .sqlite files
    
    * add a Makefile for the pipelines
    
    * add requirements
    
    * improve the pipeline a bit

---
+++ data/convert2.py
@@ -0,0 +1,13 @@
+from meza import io
+import pandas as pd
+
+tableNames = ["events", "aircraft","injury"]
+
+for tableName in tableNames:
+    dataFrame = pd.DataFrame(io.read("avall.mdb", table=tableName))
+    dataFrame.to_sql(tableName, 'sqlite:///us.sqlite', if_exists='replace', index=False)
+    dataFrame2 = pd.DataFrame(io.read("Pre2008.mdb", table=tableName))
+    dataFrame2.to_sql(tableName, 'sqlite:///us.sqlite', if_exists='append', index=False)
+
+
+
---
+++ data/download2.py
@@ -0,0 +1,19 @@
+#import wget
+import zipfile 
+from urllib import request
+import os
+
+downloads = [("https://data.ntsb.gov/avdata/FileDirectory/DownloadFile?fileID=C%3A%5Cavdata%5Cavall.zip", "avdataPost08.zip"),
+	("https://data.ntsb.gov/avdata/FileDirectory/DownloadFile?fileID=C%3A%5Cavdata%5CPre2008.zip", "avdataPre08.zip")]
+
+for url, filename in downloads:
+#	wget.download(url, filename)
+	request.urlretrieve(url, filename)
+
+with zipfile.ZipFile("avdataPost08.zip", 'r') as zip_ref:
+    zip_ref.extractall(".")
+with zipfile.ZipFile("avdataPre08.zip", 'r') as zip_ref:
+    zip_ref.extractall(".")
+
+os.remove("avdataPost08.zip")
+os.remove("avdataPre08.zip")
