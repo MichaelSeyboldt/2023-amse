49aef657b380a56e67486f8c0d4e06195553879f
M. Seyboldt <michael.seyboldt@fau.de>
2023-05-31 14:18:00 +0200

    add a clean step before full testing and fix the convert2 script

--- data/convert2.py
+++ data/convert2.py
@@ -9,12 +9,3 @@ for tableName in tableNames:
     dataFrame2 = pd.DataFrame(io.read("Pre2008.mdb", table=tableName))
     dataFrame2.to_sql(tableName, 'sqlite:///us.sqlite', if_exists='append', index=False)
 
-eventsFrame = pd.DataFrame(io.read("avall.mdb", table="events"))
-aircraftFrame = pd.DataFrame(io.read("avall.mdb", table="aircraft"))
-injuryFrame = pd.DataFrame(io.read("avall.mdb", table="injury"))
-partFrame = pd.merge(eventsFrame, aircraftFrame, how="left", on="ev_id",validate="1:m" )
-fullFrame = pd.merge(partFrame, injuryFrame, how="left", on=["ev_id","Aircraft_Key"])
-fullFrame.to_sql("fullTable", 'sqlite:///us.sqlite', if_exists='replace', index=False)
-
-
-
--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ../data
 source .venv/bin/activate
+make clean
 make
 python3 test.py
 deactivate 
