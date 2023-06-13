806478cf7af6f11d88ac99b32fb8d0b80b4fbb63
ghostyhands <ghostyhands@rommelwood.de>
2023-06-13 17:50:02 +0200

    test skript now failes properly if one of the parts fails

--- project/tests.sh
+++ project/tests.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 cd ../data
 source .venv/bin/activate
+
 make clean
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "make clean failed"
+    exit $ret
+fi
 make
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "make failed"
+    exit $ret
+fi
 python3 test.py
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "tests failed"
+    exit $ret
+fi
 deactivate 
